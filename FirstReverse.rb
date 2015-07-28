5b69e3c70d202444b49f3cf8e7dc5eab03cbab48
sjk0908 <sjk0908@users.noreply.github.com>
2015-07-28 19:14:55 +0900

    Create FirstReverse.rb

---
+++ FirstReverse.rb
@@ -0,0 +1,11 @@
+def FirstReverse(str)
+
+  # code goes here
+  # str 문장을 뒤집는다.
+  return str.reverse 
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+FirstReverse(STDIN.gets)           
